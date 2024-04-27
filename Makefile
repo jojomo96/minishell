b0f115da009486e1ce8755444983eecfe0bc68c7
flomero <49640251+flomero@users.noreply.github.com>
2024-04-27 13:03:55 +0200

    add get env

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/04/25 17:59:27 by flfische          #+#    #+#              #
-#    Updated: 2024/04/26 18:35:39 by flfische         ###   ########.fr        #
+#    Updated: 2024/04/27 12:33:46 by flfische         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -34,6 +34,7 @@ CFILES += ft_env_index.c \
 			ft_env_change.c \
 			ft_env_create_entry.c \
 			ft_env_remove.c \
+			ft_env_get.c \
 
 # UTILS
 CFILES += ft_strarr_cpy.c \
---
+++ srcs/env/ft_env_get.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_env_get.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/27 12:20:29 by flfische          #+#    #+#             */
+/*   Updated: 2024/04/27 12:43:53 by flfische         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+// TODO: Setting different error codes for different errors.
+/**
+ * @brief Get the value of an environment variable.
+ * @param env The environment variables.
+ * @param key The key of the environment variable.
+ * @return The value of the environment variable or
+ * NULL if the environment variable does not exist.
+ */
+char	*ft_get_env(char **env, char *key)
+{
+	int		i;
+	char	*entry;
+	char	*val;
+	char	**temp;
+
+	i = ft_env_index(env, key);
+	if (i == -1)
+		return (NULL);
+	entry = env[i];
+	temp = ft_split(entry, '=');
+	if (temp == NULL)
+		return (NULL);
+	val = temp[1];
+	free(temp[0]);
+	free(temp[2]);
+	free(temp);
+	return (val);
+}
