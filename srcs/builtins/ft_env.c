e7cc77568af17f503befc3d6cb6a135ddd02f47d
flomero <49640251+flomero@users.noreply.github.com>
2024-05-09 14:26:14 +0200

    standardize builtin input and give them fd

--- srcs/builtins/ft_env.c
+++ srcs/builtins/ft_env.c
@@ -6,20 +6,20 @@
 /*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 15:45:02 by flfische          #+#    #+#             */
-/*   Updated: 2024/04/26 15:50:00 by flfische         ###   ########.fr       */
+/*   Updated: 2024/05/09 14:11:14 by flfische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_env(char ***envp)
+int	ft_env(t_shell *ms, int fd_out)
 {
 	int	i;
 
 	i = 0;
-	while ((*envp)[i] != NULL)
+	while (ms->env[i] != NULL)
 	{
-		ft_putendl_fd((*envp)[i], 1);
+		ft_putendl_fd(ms->env[i], fd_out);
 		i++;
 	}
 	return (0);
