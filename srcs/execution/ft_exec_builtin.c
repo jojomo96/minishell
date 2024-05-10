c356de9fdcbe2f1437f27e5694cff05eb7f16254
flomero <49640251+flomero@users.noreply.github.com>
2024-05-10 14:57:13 +0200

    add exit code handling to builtins

--- srcs/execution/ft_exec_builtin.c
+++ srcs/execution/ft_exec_builtin.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_exec_builtins.c                                 :+:      :+:    :+:   */
+/*   ft_exec_builtin.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 11:22:13 by flfische          #+#    #+#             */
-/*   Updated: 2024/05/10 11:29:14 by flfische         ###   ########.fr       */
+/*   Updated: 2024/05/10 14:56:50 by flfische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 int	ft_exec_builtin(t_shell *ms, t_builtin builtin, char **argv, int fd_out)
 {
 	if (builtin == BUILTIN_ECHO)
-		return (ft_echo(argv, fd_out));
-	if (builtin == BUILTIN_CD)
-		return (ft_cd(ms, argv[1], fd_out));
-	if (builtin == BUILTIN_PWD)
-		return (ft_pwd(fd_out));
-	if (builtin == BUILTIN_EXPORT)
-		return (ft_export(ms, argv, fd_out));
-	if (builtin == BUILTIN_UNSET)
-		return (ft_unset(ms, argv));
-	if (builtin == BUILTIN_ENV)
-		return (ft_env(ms, fd_out));
-	if (builtin == BUILTIN_EXIT)
+		ms->exit_code = ft_echo(argv, fd_out);
+	else if (builtin == BUILTIN_CD)
+		ms->exit_code = ft_cd(ms, argv[1], fd_out);
+	else if (builtin == BUILTIN_PWD)
+		ms->exit_code = ft_pwd(fd_out);
+	else if (builtin == BUILTIN_EXPORT)
+		ms->exit_code = ft_export(ms, argv, fd_out);
+	else if (builtin == BUILTIN_UNSET)
+		ms->exit_code = ft_unset(ms, argv);
+	else if (builtin == BUILTIN_ENV)
+		ms->exit_code = ft_env(ms, fd_out);
+	else if (builtin == BUILTIN_EXIT)
 		ft_exit(ms, argv, fd_out);
-	return (0);
+	return (ms->exit_code);
 }
