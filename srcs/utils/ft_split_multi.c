a5cab9ce3c3b0dc2ec1a8e22a632aa7cb39474d3
jojomo96 <jmoritz@student.42heilbronn.de>
2024-05-14 07:49:52 +0200

    case ?

--- srcs/utils/ft_split_multi.c
+++ srcs/utils/ft_split_multi.c
@@ -6,7 +6,7 @@
 /*   By: jmoritz < jmoritz@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/13 13:15:11 by jmoritz           #+#    #+#             */
-/*   Updated: 2024/05/13 16:38:38 by jmoritz          ###   ########.fr       */
+/*   Updated: 2024/05/14 07:48:47 by jmoritz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,25 @@ char	**ft_split_on_delim(const char *str, bool(delim)(char c))
 	int		k;
 	char	**result;
 	char	*temp;
+	bool	was_qestion_mark;
 
 	result = malloc(sizeof(char *) * (ft_strlen(str) + 1));
 	temp = malloc(sizeof(char) * (ft_strlen(str) + 2));
 	i = 0;
 	j = 0;
 	k = 0;
+	was_qestion_mark = false;
 	while (str[i] != '\0')
 	{
-		if (!delim(str[i]))
+		if (!delim(str[i]) && !was_qestion_mark)
+		{
+			if(str[i] == '?')
+				was_qestion_mark = true;
 			temp[j++] = str[i++];
+		}
 		else
 		{
+			was_qestion_mark = false;
 			temp[j] = '\0';
 			if (j != 0)
 				result[k++] = ft_strdup(temp);
