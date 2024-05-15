19bf1ff15e2772bbf4febfaf20564d98dd4b4e1c
jojomo96 <jmoritz@student.42heilbronn.de>
2024-05-15 12:30:54 +0200

    make swith non static

--- includes/signals.h
+++ includes/signals.h
@@ -6,7 +6,7 @@
 /*   By: jmoritz < jmoritz@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 11:42:40 by jmoritz           #+#    #+#             */
-/*   Updated: 2024/05/15 12:16:04 by jmoritz          ###   ########.fr       */
+/*   Updated: 2024/05/15 12:30:27 by jmoritz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,7 @@
 void	ft_signals_init(void);
 void	ft_sigint_handler_heredoc(int signum);
 void	ft_sigint_handler_normal(int signum);
+void	ft_switch_to_heredoc_mode(void);
+void	ft_switch_to_normal_mode(void);
 
 #endif
--- srcs/signals/ft_signals.c
+++ srcs/signals/ft_signals.c
@@ -6,7 +6,7 @@
 /*   By: jmoritz < jmoritz@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 11:45:10 by jmoritz           #+#    #+#             */
-/*   Updated: 2024/05/15 12:26:29 by jmoritz          ###   ########.fr       */
+/*   Updated: 2024/05/15 12:30:13 by jmoritz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,13 @@ static void	set_signal_handler(int signum, void (*handler)(int))
 	}
 }
 
-static void	ft_switch_to_normal_mode(void)
+void	ft_switch_to_normal_mode(void)
 {
 	set_signal_handler(SIGINT, ft_sigint_handler_normal);
 	set_signal_handler(SIGQUIT, SIG_IGN);
 }
 
-static void	ft_switch_to_heredoc_mode(void)
+void	ft_switch_to_heredoc_mode(void)
 {
 	set_signal_handler(SIGINT, ft_sigint_handler_heredoc);
 	set_signal_handler(SIGQUIT, SIG_IGN);
