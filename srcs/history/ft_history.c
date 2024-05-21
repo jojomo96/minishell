4063b9bc3704b9b6fe2590f12a9e8dcee445e88b
jojomo96 <jmoritz@student.42heilbronn.de>
2024-05-21 18:21:50 +0200

    refactor error handeling

--- srcs/history/ft_history.c
+++ srcs/history/ft_history.c
@@ -6,17 +6,24 @@
 /*   By: jmoritz < jmoritz@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 14:08:14 by jmoritz           #+#    #+#             */
-/*   Updated: 2024/05/21 18:15:07 by jmoritz          ###   ########.fr       */
+/*   Updated: 2024/05/21 18:21:06 by jmoritz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	handle_error(const char *msg, int fd)
+{
+	perror(msg);
+	if (fd >= 0)
+		close(fd);
+}
+
 void	ft_history_add(char *input)
 {
-	int			fd;
+	int	fd;
 
-	if (input == NULL || strlen(input) == 0)
+	if (!input || strlen(input) == 0)
 		return ;
 	add_history(input);
 	fd = open(HISTORY_FILE, O_WRONLY | O_APPEND | O_CREAT, 0644);
@@ -25,16 +32,9 @@ void	ft_history_add(char *input)
 		ft_putstr_fd("Error: could not open history file\n", STDERR_FILENO);
 		return ;
 	}
-	if (write(fd, input, strlen(input)) < 0)
+	if (write(fd, input, strlen(input)) < 0 || write(fd, "\n", 1) < 0)
 	{
-		perror("Error writing to history file");
-		close(fd);
-		return ;
-	}
-	if (write(fd, "\n", 1) < 0)
-	{
-		perror("Error writing to history file");
-		close(fd);
+		handle_error("Error writing to history file", fd);
 		return ;
 	}
 	close(fd);
@@ -44,15 +44,17 @@ static void	ft_remove_newline(char *line)
 {
 	size_t	len;
 
+	if (!line)
+		return ;
 	len = strlen(line);
-	if (line[len - 1] == '\n')
+	if (len > 0 && line[len - 1] == '\n')
 		line[len - 1] = '\0';
 }
 
 void	ft_history_init(void)
 {
-	int			fd;
-	char		*line;
+	int		fd;
+	char	*line;
 
 	fd = open(HISTORY_FILE, O_RDONLY | O_CREAT, 0644);
 	if (fd < 0)
