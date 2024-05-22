755f626b178602f16596c9b1f9003fd642cc8636
flomero <49640251+flomero@users.noreply.github.com>
2024-05-22 12:53:19 +0200

    add upercas builtin handling

--- srcs/builtins/ft_is_builtin.c
+++ srcs/builtins/ft_is_builtin.c
@@ -6,27 +6,37 @@
 /*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 11:28:04 by flfische          #+#    #+#             */
-/*   Updated: 2024/05/10 11:28:44 by flfische         ###   ########.fr       */
+/*   Updated: 2024/05/22 12:51:34 by flfische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static char	make_lower(unsigned int i, char c)
+{
+	(void)i;
+	return (ft_tolower(c));
+}
+
 t_builtin	ft_is_builtin(char *cmd)
 {
-	if (!ft_strcmp(cmd, "echo"))
+	char	*lower;
+
+	lower = ft_strmapi(cmd, &make_lower);
+	if (!ft_strcmp(lower, "echo"))
 		return (BUILTIN_ECHO);
-	if (!ft_strcmp(cmd, "cd"))
+	if (!ft_strcmp(lower, "cd"))
 		return (BUILTIN_CD);
-	if (!ft_strcmp(cmd, "pwd"))
+	if (!ft_strcmp(lower, "pwd"))
 		return (BUILTIN_PWD);
-	if (!ft_strcmp(cmd, "export"))
+	if (!ft_strcmp(lower, "export"))
 		return (BUILTIN_EXPORT);
-	if (!ft_strcmp(cmd, "unset"))
+	if (!ft_strcmp(lower, "unset"))
 		return (BUILTIN_UNSET);
-	if (!ft_strcmp(cmd, "env"))
+	if (!ft_strcmp(lower, "env"))
 		return (BUILTIN_ENV);
-	if (!ft_strcmp(cmd, "exit"))
+	if (!ft_strcmp(lower, "exit"))
 		return (BUILTIN_EXIT);
+	free(lower);
 	return (NONE);
 }
