c580ee78c3da647c0ff5aafa0c3b4cd7c9691ca1
flomero <49640251+flomero@users.noreply.github.com>
2024-05-24 18:13:10 +0200

    fix double printed err message on export -

--- srcs/builtins/ft_export.c
+++ srcs/builtins/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 16:40:57 by flfische          #+#    #+#             */
-/*   Updated: 2024/05/22 18:14:04 by flfische         ###   ########.fr       */
+/*   Updated: 2024/05/24 18:11:10 by flfische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,16 +39,21 @@ static void	ft_export_print(char **exp, int fd_out)
 	}
 }
 
-static int	export_error(char **tmp)
+static int	export_error(char **tmp, bool print_error)
 {
 	if (!tmp)
 		return (ft_print_error(strerror(errno), NULL, NULL), 1);
 	if (!ft_valid_env_key(tmp[0]))
 	{
 		if (tmp[0] && tmp[0][0] == '-')
-			return (ft_print_error_env("invalid option", "export", tmp[0]), 2);
-		return (ft_print_error_env("not a valid identifier", "export", tmp[0]),
-			1);
+		{
+			if (print_error)
+				ft_print_error_env("invalid option", "export", tmp[0]);
+			return (2);
+		}
+		if (print_error)
+			ft_print_error_env("not a valid identifier", "export", tmp[0]);
+		return (1);
 	}
 	return (0);
 }
@@ -67,8 +72,8 @@ int	ft_export(t_shell *ms, char **argv, int fd_out)
 		if (ft_strchr(argv[i], '='))
 		{
 			tmp = ft_split(argv[i], '=');
-			if (export_error(tmp))
-				return (ft_strarr_free(tmp), export_error(tmp));
+			if (export_error(tmp, true))
+				return (ft_strarr_free(tmp), export_error(tmp, false));
 			ft_env_set_both(ms, tmp[0], tmp[1]);
 			ft_strarr_free(tmp);
 		}
