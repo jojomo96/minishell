a64e8a88b0c7c78d0918354943f4c3e25c9b3195
flomero <49640251+flomero@users.noreply.github.com>
2024-05-25 11:20:30 +0200

    fix redir out

--- srcs/execution/ft_redirect.c
+++ srcs/execution/ft_redirect.c
@@ -6,18 +6,38 @@
 /*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/14 11:40:09 by flfische          #+#    #+#             */
-/*   Updated: 2024/05/24 15:47:15 by flfische         ###   ########.fr       */
+/*   Updated: 2024/05/25 11:18:09 by flfische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	set_exit_status_err(t_ast_node *node)
+static void	set_exit_err(t_ast_node *node)
 {
 	node->exit_status = 1;
 	node->u_data.s_node.right->exit_status = 1;
 	node->u_data.s_node.left->exit_status = 1;
 	ft_get_shell()->exit_code = 1;
+}
+
+static int	set_left_fd(t_ast_node *node, int fd)
+{
+	t_ast_node	*left;
+
+	left = node->u_data.s_node.left;
+	while (left->type != AST_TYPE_LEAF)
+	{
+		if (left->type == AST_TYPE_NODE && left->u_data.type == OP_AND)
+			left = left->u_data.s_node.right;
+		else
+			left = left->u_data.s_node.left;
+	}
+	if (left->u_data.leaf.fd_out == STDOUT_FILENO)
+	{
+		left->u_data.leaf.fd_out = fd;
+		return (1) ;
+	}
+	return (0);
 }
 
 static int	handle_ambigous_redirect(t_ast_node *node)
@@ -53,19 +73,21 @@ int	ft_exec_redirect_out(t_shell *ms, t_ast_node *node)
 	std_out = dup(STDOUT_FILENO);
 	fd = open(node->u_data.s_node.right->u_data.leaf.argv[0],
 			O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	set_left_fd(node, fd);
 	if (fd == -1)
-		return (set_exit_status_err(node), ft_print_error(strerror(errno), NULL,
-				NULL), 1);
+		return (set_exit_err(node), ft_print_error(strerror(errno), 0, 0), 1);
+	if (node->u_data.s_node.left->type == AST_TYPE_LEAF && node->u_data.\
+		s_node.left->u_data.leaf.fd_out != fd && close(fd) != -1)
+		fd = node->u_data.s_node.left->u_data.leaf.fd_out;
 	if (dup2(fd, STDOUT_FILENO) == -1)
-		return (close(fd), set_exit_status_err(node),
+		return (close(fd), set_exit_err(node),
 			ft_print_error(strerror(errno), NULL, NULL), 1);
-	close(fd);
 	ret = ft_execute(ms, node->u_data.s_node.left);
 	if (dup2(std_out, STDOUT_FILENO) == -1)
 		return (ft_print_error(strerror(errno), NULL, NULL), 1);
 	close(std_out);
 	if (node->u_data.s_node.left->exit_status == 1)
-		set_exit_status_err(node);
+		set_exit_err(node);
 	return (ret);
 }
 
@@ -81,10 +103,10 @@ int	ft_exec_append_out(t_shell *ms, t_ast_node *node)
 	fd = open(node->u_data.s_node.right->u_data.leaf.argv[0],
 			O_WRONLY | O_CREAT | O_APPEND, 0644);
 	if (fd == -1)
-		return (set_exit_status_err(node), ft_print_error(strerror(errno),
+		return (set_exit_err(node), ft_print_error(strerror(errno),
 				node->u_data.s_node.right->u_data.leaf.argv[0], NULL), 1);
 	if (dup2(fd, STDOUT_FILENO) == -1)
-		return (close(fd), set_exit_status_err(node),
+		return (close(fd), set_exit_err(node),
 			ft_print_error(strerror(errno), NULL, NULL), 1);
 	close(fd);
 	ret = ft_execute(ms, node->u_data.s_node.left);
@@ -92,7 +114,7 @@ int	ft_exec_append_out(t_shell *ms, t_ast_node *node)
 		return (ft_print_error(strerror(errno), NULL, NULL), 1);
 	close(std_out);
 	if (node->u_data.s_node.left->exit_status == 1)
-		set_exit_status_err(node);
+		set_exit_err(node);
 	return (ret);
 }
 
@@ -107,18 +129,18 @@ int	ft_exec_redirect_in(t_shell *ms, t_ast_node *node)
 	std_in = dup(STDIN_FILENO);
 	fd = open(node->u_data.s_node.right->u_data.leaf.argv[0], O_RDONLY);
 	if (fd == -1)
-		return (set_exit_status_err(node), ft_print_error(strerror(errno),
+		return (set_exit_err(node), ft_print_error(strerror(errno),
 				node->u_data.s_node.right->u_data.leaf.argv[0], NULL), 1);
 	if (dup2(fd, STDIN_FILENO) == -1)
-		return (close(fd), set_exit_status_err(node),
+		return (close(fd), set_exit_err(node),
 			ft_print_error(strerror(errno), NULL, NULL), 1);
 	close(fd);
 	ret = ft_execute(ms, node->u_data.s_node.left);
 	if (dup2(std_in, STDIN_FILENO) == -1)
-		return (set_exit_status_err(node), ft_print_error(strerror(errno), NULL,
+		return (set_exit_err(node), ft_print_error(strerror(errno), NULL,
 				NULL), 1);
 	close(std_in);
 	if (node->u_data.s_node.left->exit_status == 1)
-		set_exit_status_err(node);
+		set_exit_err(node);
 	return (ret);
 }
